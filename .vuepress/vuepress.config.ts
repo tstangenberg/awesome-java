8a27ba7fa0e0ebe74c38e15d46a8417a289c9f2e
Thorben Stangenberg <thorben@stangenberg.net>
2023-07-04 20:45:53 +0000

    vuepress config

---
+++ .vuepress/vuepress.config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'Awesome Java',
+  description: 'Awesome List for the programming language Java.',
+  plugins: [],
+})
