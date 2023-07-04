ae9c53f2a9a61ead515f3d91ac6bcce7a39d9269
Thorben Stangenberg <thorben@stangenberg.net>
2023-07-04 21:17:15 +0000

    google analytics

--- .vuepress/vuepress.config.ts
+++ .vuepress/vuepress.config.ts
@@ -1,10 +1,15 @@
 import { defineUserConfig } from 'vuepress'
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Awesome Java',
   description: 'A curated list of awesome Java frameworks, libraries and software.',
-  plugins: [],
+  plugins: [
+    googleAnalyticsPlugin({
+      id: 'G-L5SPYCH1EJ',
+    })
+  ],
   head: [
     ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
     ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }]
