b6159c9888d466171e00c7f89fb4613d4496f123
Thorben Stangenberg <thorben@stangenberg.net>
2023-07-07 06:35:03 +0000

    docsearch

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from 'vuepress'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 
 export default defineUserConfig({
   lang: 'en-US',
@@ -8,7 +9,22 @@ export default defineUserConfig({
   plugins: [
     googleAnalyticsPlugin({
       id: 'G-L5SPYCH1EJ',
-    })
+    }),
+    docsearchPlugin({
+      appId: 'E0Z0E9JPF2',
+      apiKey: '15939139fae92452e3e9cbc22994e4d5',
+      indexName: 'awesome-java',
+      locales: {
+        '/': {
+          placeholder: 'Search here',
+          translations: {
+            button: {
+              buttonText: 'Search',
+            },
+          },
+        },
+      },
+    }),
   ],
   head: [
     ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
